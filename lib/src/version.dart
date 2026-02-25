f934cc1411bb0867139797f8f40519174cfad068
blackabass@gmail.com <16978751+blackabass@users.noreply.github.com>
2026-02-25 09:47:23 +0000

    release: v0.11.1

--- lib/src/version.dart
+++ lib/src/version.dart
@@ -6,4 +6,4 @@
 /// To update the version:
 /// 1. Update `version:` in `pubspec.yaml`
 /// 2. The pre-commit hook will automatically sync this file
-const String polyfencePluginVersion = '0.11.0';
+const String polyfencePluginVersion = '0.11.1';
